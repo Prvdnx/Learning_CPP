5da6237546d81d740130ceb4e4d50c4e4d6c24bb
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-08 15:46:13 +0000

    commit

---
+++ Basics/hello_world.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello World!" << endl;
+//     return 0;
+// }
+
+
+// int main(int argc , char *argv[])
+// {
+//     std::cout<< "Hello World!" <<std::endl;
+//     return 0;
+// }
+
+
+int main()
+{
+    int a, b, c;
+    a = 10;
+    b = 20;
+    c = a + b;
+    std::cout << c << std::endl;
+
+    std::cout << "Hello World!\n";
+}
