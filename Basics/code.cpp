e60e46793ff0f31e641b1acace1e77793479e3e0
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-08 18:43:49 +0000

    c

---
+++ Basics/code.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sum = 0;
+    int A[] = {1,2,5,8,9};
+
+    for(auto x : A)
+    {
+        sum = sum + x;
+    }
+
+    cout << sum << endl;
+    return (0);
+}
