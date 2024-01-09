d62c6365773871c2cd26df4f0e0ab24a9feb1329
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-09 01:42:53 +0000

    commit

--- C++ Tutorials/03_cin.cpp
+++ C++ Tutorials/03_cin.cpp
@@ -3,21 +3,62 @@
 
 using namespace std;
 
+// int main()
+// {
+//     int x;
+//     cout << "Enter an Integer: ";
+//     cin >> x;
+//     // if invalid input is entered, "cin" will be stuck with an error that affects other calls to "cin" after that but there's way to fix that
+//     cout << "You entered the Integer " << x << endl;
+
+
+//     // Calculate area
+//     double height = 0;
+//     double width = 0;
+//     double area = 0;
+
+//     cout << "Enter height and width (separated by space) : ";
+//     cin >> height >> width;
+//     area = height * width;
+//     cout << height << " x " << width << " = " << area << endl;
+
+//     return (0);
+// }
+
+
 int main()
 {
-    // int x;
-    // cout << "Enter an Integer: ";
-    // cin >> x;
-    // cout << "You entered the Integer " << x << endl;
+/*    int x;
+    cout << "Enter an Integer: ";
+    if (cin >> x)
+    {
+    cout << "You entered the Integer " << x << endl;
+    }
+    else
+    {
+        cin.clear();
+        cout << "Invalid Input" << endl;
+    }
 
+    cin.ignore(1000, '\n');
+
+
+    // Calculate area
     double height = 0;
     double width = 0;
     double area = 0;
-                    // Calculate area
+
     cout << "Enter height and width (separated by space) : ";
     cin >> height >> width;
     area = height * width;
-    cout << height << " x " << width << " = " << area << endl;
+    cout << height << " x " << width << " = " << area << endl; */
+
+    // Working with string
+    string name;
+    cout << "Enter your name? ";
+    //cin >> name; //cin reads only the first string if more than one is entered.
+    getline(cin, name); //This reads all string entered if more than one is entered.
+    cout << "Hello " << name << "!" << endl;
 
     return (0);
 }
---
+++ C++ Tutorials/05_if_statement.cpp
@@ -0,0 +1,78 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // if (5 == 5)
+    // {
+    //     cout << "5 does equal 5!" << endl;
+    // }
+    // cout << "If Statement Done!" << endl;
+
+
+    // int price = 100;
+    // if (price == 100)
+    // {
+    //     cout << "price does equal 100!" << endl;
+    // }
+    // cout << "If Statement Done!" << endl;
+
+
+    // int price = 90;
+    // if (price <= 90)
+    // {
+    //     cout << "price <= 90!" << endl;
+    //     cout << "Good deal!" << endl;
+    // }
+    // cout << "If Statement Done!" << endl;
+
+
+    // int price = 150;
+    // if (price <= 90)
+    // {
+    //     cout << "price <= 90!" << endl;
+    //     cout << "Good deal!" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not a good deal" << endl;
+    // }
+    // cout << "If Statement Done!" << endl;
+
+
+    // int price = 120;
+    // if (price <= 90)
+    // {
+    //     cout << "price <= 90!" << endl;
+    //     cout << "Good deal!" << endl;
+    // }
+    // else if (price >= 200)
+    // {
+    //     cout << "Very bad price!" << endl;
+    // }
+    // else
+    // {
+    //     cout << "Not a good deal" << endl;
+    // }
+    // cout << "If Statement Done!" << endl;
+
+
+    int price = 40;
+    if (price <= 90)
+    {
+        cout << "price <= 90!" << endl;
+        cout << "Good deal!" << endl;
+    }
+    else if (price <= 50)
+        cout << "Amazing Deal!" << endl;
+    else if (price >= 200)
+    {
+        cout << "Very bad price!" << endl;
+    }
+    else
+        cout << "Not a good deal" << endl;
+    
+    cout << "If Statement Done!" << endl;
+}
