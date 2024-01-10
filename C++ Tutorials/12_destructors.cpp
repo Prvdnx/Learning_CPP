5cce9613c080310274d9e08102237129af3e34ff
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-10 01:28:46 +0000

    commit

--- C++ Tutorials/12_destructors.cpp
+++ C++ Tutorials/12_destructors.cpp
@@ -3,7 +3,46 @@
 
 using namespace std;
 
+class Number
+{
+    private:
+        // double number;
+        double *number;
+
+    public:
+
+        Number(double num)
+        {
+            // number = num;
+            number = (double *) malloc(sizeof(double));
+            *number = num;
+            cout << "Constructor executing!" << endl;
+            // cout << "Number: " << number << endl << endl;
+            cout << "Number: " << *number << endl << endl;
+        }
+
+        ~Number()
+        {
+            cout << "Destructor executing!" << endl;
+            // cout << "NUmber: " << number << endl << endl;
+            cout << "Number: " << *number << endl << endl;
+            free(number);
+        }
+};
+
+void test()
+{
+    Number six(6);
+}
+
 int main()
 {
-    
+    Number *five = new Number(5);
+    delete five;
+
+    test();
+
+    Number seven(7);
+
+    return (0);
 }
