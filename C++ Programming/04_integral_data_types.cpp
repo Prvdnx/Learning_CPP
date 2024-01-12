c5efda52d0f8f48159c082c285a54cc669009b57
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-12 13:14:53 +0000

    update 04_integral_data_types.cpp

--- C++ Programming/04_integral_data_types.cpp
+++ C++ Programming/04_integral_data_types.cpp
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <climits>
 
+//\\//\\ INT DATA TYPES ====================================
+
+// int main()
+// {
+//     short a; // 16 bits  (1 byte is 8 bits)
+//     int b; // at least 16 bits but usually 32
+//     long c; // at least 32 bits
+//     long long d; // at least 64 bits
+
+//     // short <= int <= long <= long long
+
+//     unsigned short aa;
+//     unsigned int bb;
+//     unsigned long cc;
+//     unsigned long long dd;
+
+//     std::cout << sizeof(long long) << std::endl;
+//     std::cout << ULLONG_MAX << std::endl; // using <climits>
+
+//     return (0);
+// }
+
+
+
+//\\//\\ INT DATA TYPES ====================================
+
 int main()
 {
-    short a; // 16 bits  (1 byte is 8 bits)
-    int b; // at least 16 bits but usually 32
-    long c; // at least 32 bits
-    long long d; // at least 64 bits
+    char x = 65;
+    //unsigned char c = 129;
 
-    // short <= int <= long <= long long
-
-    unsigned short aa;
-    unsigned int bb;
-    unsigned long cc;
-    unsigned long long dd;
-
-    std::cout << sizeof(long long) << std::endl;
-    std::cout << sizeof(long long) << std::endl; // <climits>
+    std::cout << x << std::endl;
+    std::cout << (int) x << std::endl;
 
     return (0);
 }
+
+
