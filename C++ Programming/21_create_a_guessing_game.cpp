e7ad12a56626a96678b90a13ab2fcb07f96550b8
Prvdnx <129741476+Prvdnx@users.noreply.github.com>
2024-01-15 23:45:01 +0000

    update 21_create_a_guessing_game.cpp

--- C++ Programming/21_create_a_guessing_game.cpp
+++ C++ Programming/21_create_a_guessing_game.cpp
@@ -4,6 +4,28 @@
 void play_game()
 {
     std::cout << "Game is being played!\n";
+    int random = rand() % 251;
+    // std::cout << "random" << std::endl; //
+    std::cout << "Guess a number: ";
+
+    while(true)
+    {
+        int guess;
+        std::cin >> guess;
+        if(guess == random)
+        {
+            std::cout << "You win!\n";
+            break;
+        }
+        else if(guess < random)
+        {
+            std::cout << "Too low\n";
+        }
+        else
+        {
+            std::cout << "Too high\n";
+        }
+    }
 }
 
 int main()
